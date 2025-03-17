046a55ed430e1135115f743612dd125a249306d3
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-17 19:10:51 +0100

    Add MappingProfile

---
+++ DevHobby.CourseFlow.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DevHobby.CourseFlow.Application.Features.Courses;
+using DevHobby.CourseFlow.Domain.Entities;
+
+namespace DevHobby.CourseFlow.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Course, CourseListVm>().ReverseMap();
+    }
+}
