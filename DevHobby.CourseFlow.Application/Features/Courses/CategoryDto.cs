350c050c68d7411b0228c32e2aa3c8e1e4cbc3d6
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-17 19:13:00 +0100

    Add CourseDetailVm

---
+++ DevHobby.CourseFlow.Application/Features/Courses/CategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DevHobby.CourseFlow.Application.Features.Courses;
+
+public class CategoryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- DevHobby.CourseFlow.Application/Features/Courses/CourseDetailVm.cs
+++ DevHobby.CourseFlow.Application/Features/Courses/CourseDetailVm.cs
@@ -1,6 +1,14 @@
-﻿namespace DevHobby.CourseFlow.Application.Features.Courses
+﻿namespace DevHobby.CourseFlow.Application.Features.Courses;
+
+public class CourseDetailVm
 {
-    public class CourseDetailVm
-    {
-    }
+    public Guid CourseId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public string Author { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public DateTime PublicationDate { get; set; }
+    public Guid CategoryId { get; set; }
+    public CategoryDto Category { get; set; } = default!;
 }
