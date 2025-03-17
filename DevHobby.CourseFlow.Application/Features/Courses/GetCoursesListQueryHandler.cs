60120abb2235177e8dfc5bc755839a00eb636a2f
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-17 19:09:39 +0100

    Add GetCoursesListQueryHandler

---
+++ DevHobby.CourseFlow.Application/Features/Courses/GetCoursesListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DevHobby.CourseFlow.Application.Contracts.Persistence;
+using DevHobby.CourseFlow.Domain.Entities;
+using MediatR;
+
+namespace DevHobby.CourseFlow.Application.Features.Courses;
+
+public class GetCoursesListQueryHandler : IRequestHandler<GetCoursesListQuery, List<CourseListVm>>
+{
+    private readonly IMapper _mapper;
+    private readonly IAsyncRepository<Course> _asyncRepository;
+
+    public GetCoursesListQueryHandler(IMapper mapper, IAsyncRepository<Course> asyncRepository)
+    {
+        _mapper = mapper;
+        _asyncRepository = asyncRepository;
+    }
+
+    public async Task<List<CourseListVm>> Handle(GetCoursesListQuery request, CancellationToken cancellationToken)
+    {
+        var allCourses = (await _asyncRepository.ListAllAsync()).OrderBy(x => x.PublicationDate);
+
+        return _mapper.Map<List<CourseListVm>>(allCourses);
+    }
+}
