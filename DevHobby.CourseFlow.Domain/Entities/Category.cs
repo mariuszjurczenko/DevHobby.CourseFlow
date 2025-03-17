19cc6ba345ed58b39033a6806d56b6d1f295925c
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-17 18:56:32 +0100

    Add Category Entities

---
+++ DevHobby.CourseFlow.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace DevHobby.CourseFlow.Domain.Entities;
+
+public class Category
+{
+    public Guid CategoryId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Course>? Courses { get; set; } = new List<Course>();
+}
