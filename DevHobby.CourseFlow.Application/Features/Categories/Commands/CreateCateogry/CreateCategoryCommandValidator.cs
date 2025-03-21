82dacd46647af0316825458312235db227aaa19d
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-21 11:11:31 +0100

    Add CreateCategoryCommandValidator

---
+++ DevHobby.CourseFlow.Application/Features/Categories/Commands/CreateCateogry/CreateCategoryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace DevHobby.CourseFlow.Application.Features.Categories.Commands.CreateCateogry;
+
+public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters");
+    }
+}
