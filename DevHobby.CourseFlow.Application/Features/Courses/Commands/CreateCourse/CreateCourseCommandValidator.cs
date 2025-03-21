1bcf61376ea72d0ca7808347a0b58e7360fe4ccb
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-21 08:21:53 +0100

    Add CreateCourseCommandValidator

---
+++ DevHobby.CourseFlow.Application/Features/Courses/Commands/CreateCourse/CreateCourseCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace DevHobby.CourseFlow.Application.Features.Courses.Commands.CreateCourse;
+
+public class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
+{
+    public CreateCourseCommandValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+        RuleFor(p => p.Price)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .GreaterThan(0).WithMessage("{PropertyName} must be greater than 0.");
+        RuleFor(p => p.PublicationDate)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .GreaterThan(DateTime.Now);
+    }
+}
