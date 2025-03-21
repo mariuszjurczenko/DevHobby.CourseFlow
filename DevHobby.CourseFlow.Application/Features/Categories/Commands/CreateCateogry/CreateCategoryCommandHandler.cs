8fb8875a8031823a6ee6529649efc2285595b59b
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-21 11:36:30 +0100

    Add CreateCategoryCommandHandler

---
+++ DevHobby.CourseFlow.Application/Features/Categories/Commands/CreateCateogry/CreateCategoryCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using DevHobby.CourseFlow.Application.Contracts.Persistence;
+using DevHobby.CourseFlow.Domain.Entities;
+using MediatR;
+
+namespace DevHobby.CourseFlow.Application.Features.Categories.Commands.CreateCateogry;
+
+public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, CreateCategoryCommandResponse>
+{
+    private readonly IMapper _mapper;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CreateCategoryCommandHandler(IMapper mapper, ICategoryRepository categoryRepository)
+    {
+        _mapper = mapper;
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<CreateCategoryCommandResponse> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var createCategoryCommandResponse = new CreateCategoryCommandResponse();
+        var validator = new CreateCategoryCommandValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.Errors.Count > 0)
+        {
+            createCategoryCommandResponse.Success = false;
+            createCategoryCommandResponse.ValidationErrors = new List<string>();
+
+            foreach (var error in validationResult.Errors)
+            {
+                createCategoryCommandResponse.ValidationErrors.Add(error.ErrorMessage);
+            }
+        }
+
+        if (createCategoryCommandResponse.Success)
+        {
+            var category = new Category() { Name = request.Name };  
+            category = await _categoryRepository.AddAsync(category);
+            createCategoryCommandResponse.Category = _mapper.Map<CreateCategoryDto>(category);
+        }
+        return createCategoryCommandResponse;
+    }
+}
