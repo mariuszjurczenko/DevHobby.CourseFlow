bd3e7a4771eb31e5829add9f4100093fb0cd74e2
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-25 10:39:24 +0100

    Add CourseConfiguration, CategoryConfiguration

---
+++ DevHobby.CourseFlow.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DevHobby.CourseFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevHobby.CourseFlow.Persistence.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
---
+++ DevHobby.CourseFlow.Persistence/Configurations/CourseConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DevHobby.CourseFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevHobby.CourseFlow.Persistence.Configurations;
+
+public class CourseConfiguration : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(e => e.PublicationDate)
+             .IsRequired();
+
+        builder.Property(e => e.Price)
+             .IsRequired();
+    }
+}
