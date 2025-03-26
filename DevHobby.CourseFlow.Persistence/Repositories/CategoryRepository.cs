1ef039cf4dcd4a2ee31697e1926fb3f3c3f257ee
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-26 15:53:48 +0100

    Add CategoryRepository

---
+++ DevHobby.CourseFlow.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using DevHobby.CourseFlow.Application.Contracts.Persistence;
+using DevHobby.CourseFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHobby.CourseFlow.Persistence.Repositories;
+
+public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(DevHobbyDbContext dbContext) : base(dbContext)
+    {       
+    }
+
+    public async Task<List<Category>> GetCategoriesWithCourses(bool includePlannedCourses)
+    {
+        var allCategories = await _dbContext.Categories.Include(c => c.Courses).ToListAsync();
+
+        if (!includePlannedCourses)
+        {
+            allCategories.ForEach(c => c.Courses.ToList().RemoveAll(c => c.PublicationDate > DateTime.Today));
+        }
+
+        return allCategories;
+    }
+}
