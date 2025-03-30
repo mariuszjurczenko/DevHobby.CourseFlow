bcbb227cbd9b1029bfdbffdb1d9cebde16c3fdb1
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-30 17:22:33 +0200

    Add SendEmailAsync from CreateCourseCommandHandler

--- DevHobby.CourseFlow.Application/Features/Courses/Commands/CreateCourse/CreateCourseCommandHandler.cs
+++ DevHobby.CourseFlow.Application/Features/Courses/Commands/CreateCourse/CreateCourseCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using DevHobby.CourseFlow.Application.Contracts.Infrastructure;
 using DevHobby.CourseFlow.Application.Contracts.Persistence;
 using DevHobby.CourseFlow.Application.Exceptions;
+using DevHobby.CourseFlow.Application.Models;
 using DevHobby.CourseFlow.Domain.Entities;
 using MediatR;
 
@@ -10,11 +12,13 @@ public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, G
 {
     private readonly IMapper _mapper;
     private readonly ICourseRepository _courseRepository;
+    private readonly IEmailService _emailService;
 
-    public CreateCourseCommandHandler(IMapper mapper, ICourseRepository courseRepository)
+    public CreateCourseCommandHandler(IMapper mapper, ICourseRepository courseRepository, IEmailService emailService)
     {
         _mapper = mapper;
         _courseRepository = courseRepository;
+        _emailService = emailService;
     }
 
     public async Task<Guid> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
@@ -28,6 +32,22 @@ public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, G
         var course = _mapper.Map<Course>(request);
 
         course = await _courseRepository.AddAsync(course);
+
+        var email = new Email()
+        {
+            To = "mariusz@dev-hobby.pl",
+            Body = $"A new course was created: {request}",
+            Subject = "A new course was created"
+        };
+
+        try
+        {
+            await _emailService.SendEmailAsync(email);
+        }
+        catch (Exception)
+        {
+            // dodamy tutaj logowanie
+        }
 
         return course.CourseId;
     }
