9b677dd7ccc12385dab959417fa87c1a22c4a43f
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-30 18:11:41 +0200

    Add InfrastructureServiceRegistration

---
+++ DevHobby.CourseFlow.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using DevHobby.CourseFlow.Application.Contracts.Infrastructure;
+using DevHobby.CourseFlow.Application.Models;
+using DevHobby.CourseFlow.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevHobby.CourseFlow.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
