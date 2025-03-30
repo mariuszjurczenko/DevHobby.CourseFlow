a13f0c1d461c98ef7381820d99b65278d72a1fce
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-30 12:20:15 +0200

    Add PersistenceServiceRegistration

---
+++ DevHobby.CourseFlow.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using DevHobby.CourseFlow.Application.Contracts.Persistence;
+using DevHobby.CourseFlow.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevHobby.CourseFlow.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(
+                            this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DevHobbyDbContext>(options =>
+           options.UseSqlite(configuration.GetConnectionString("DevHobbyCourseFlowConnectionString")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ICourseRepository, CourseRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        return services;
+    }
+}
