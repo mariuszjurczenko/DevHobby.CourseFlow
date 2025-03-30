30abe791ee062fc9aa73fe6c7baf056a6b4b7be7
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-03-30 16:40:04 +0200

    Add IEmailService

---
+++ DevHobby.CourseFlow.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using DevHobby.CourseFlow.Application.Models;
+
+namespace DevHobby.CourseFlow.Application.Contracts.Infrastructure;
+
+public interface IEmailService
+{
+    Task<bool> SendEmailAsync(Email email);
+}
